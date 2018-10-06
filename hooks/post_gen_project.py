0f724f1c77a599e1b3cef8a373b7a58c17124bb0
Marcy Rattner <marcy.rattner@gmail.com>
2018-10-06 14:08:22 +1300

    Remove unnecessary hooks

--- hooks/post_gen_project.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    import os
-
-    PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-    
-    if "{{ cookiecutter.use_bulma_and_sass|lower }}" != "y":
-        os.rmdir(os.path.join(PROJECT_DIRECTORY, "resources", "scss"))
